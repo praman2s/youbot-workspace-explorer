8e0601d4a6e14bfd5e9fdff9e7f5aec9ee850cec
Arn-O <rnd.bertrand@gmail.com>
2013-09-01 22:42:27 +0200

    loops

--- wsexplorer.py
+++ wsexplorer.py
@@ -7,12 +7,11 @@
 General description
 '''
 
-import numpy as np
 import math as m
 
 # Global constants
 PI = 3.1415926535897931
-DELTA = 0.1
+DELTA = 0.05
 
 THETA_1_MIN = 0.0
 THETA_1_MAX = (155.0 / 180) * PI
@@ -31,17 +30,35 @@ A_2 = 0.135
 A_3 = 0.218
 
 def main():
+
     nb_iter_1 = int((THETA_1_MAX - THETA_1_MIN) / DELTA) + 1
     nb_iter_2 = int((THETA_2_MAX - THETA_2_MIN) / DELTA) + 1
     nb_iter_3 = int((THETA_3_MAX - THETA_3_MIN) / DELTA) + 1
-    print "Nb of iteration for theta 1:", nb_iter_1
-    print "Nb of iteration for theta 2:", nb_iter_2
-    print "Nb of iteration for theta 3:", nb_iter_3
-    print "Total number of steps:", nb_iter_1 * nb_iter_2 * nb_iter_3
+    nb_iter = nb_iter_1 * nb_iter_2 * nb_iter_3
+    current_iter = 0
 
     for i in range(nb_iter_1):
         theta_1 = THETA_1_MIN + DELTA * i
-    
+        x_1 = A_1 * m.cos(theta_1)
+        y_1 = A_1 * m.cos(theta_1)
+        ut_1 = theta_1
 
+        for j in range(nb_iter_2):
+            theta_2 = THETA_2_MIN + DELTA * j
+            x_12 = x_1 + A_2 * m.cos(theta_1 + theta_2)
+            y_12 = y_1 + A_2 * m.sin(theta_1 + theta_2)
+            ut_12 = ut_1 + theta_2
+
+            for k in range(nb_iter_3):
+                current_iter += 1
+
+                theta_3 = THETA_3_MIN + DELTA * k
+                x_123 = x_12 + A_3 * m.cos(theta_1 + theta_2 + theta_3)
+                y_123 = y_12 + A_3 * m.sin(theta_1 + theta_2 + theta_3)
+                ut_123 = ut_12 + theta_3
+
+                print ('Iter: %5i / % 5i - ' % (current_iter, nb_iter)),
+                print ('t1: %.2f t2: %.2f t3: %.2f - x: %.3f y: %.3f' %                     (theta_1, theta_2, theta_3, x_123, y_123))
+                
 if __name__ == '__main__':
     main()
